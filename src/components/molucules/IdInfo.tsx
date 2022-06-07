1bc770d6d48d470e7dd91f4cdbb617888951368d
KonishiKenji <konishi.b0engineer@gmail.com>
2022-06-07 23:03:20 +0900

    feat: PersonalAccessTokenをinputから取得

--- src/components/molucules/IdInfo.tsx
+++ src/components/molucules/IdInfo.tsx
@@ -3,11 +3,12 @@ import './IdInfo.css';
 
 interface State {
   idInfo: string;
+  inputPersonalAccessToken: string;
 }
 class IdInfo extends React.Component<{}, State> {
   constructor(props: any) {
     super(props);
-    this.state = { idInfo: '未取得' };
+    this.state = { idInfo: '未取得', inputPersonalAccessToken: '' };
     this.handleClick = this.handleClick.bind(this);
   }
   label = 'ID取得';
@@ -15,25 +16,47 @@ class IdInfo extends React.Component<{}, State> {
   // MEMO：ひとまず直書き。今後envに格納、もしくはinput.valueにする予定
   timetreePersonalAccessTokens =
     'S1rJHodOSNCVTUOuHmfKEOS0cxk4JcedcIG0jOG3Pd6ppSqv';
+
   handleClick(process: string) {
+    if (!this.state.inputPersonalAccessToken) {
+      return console.log('inputValue is unset');
+    }
     fetch(`${this.timetreeApi}/${process}`, {
       headers: {
-        Authorization: `Bearer ${this.timetreePersonalAccessTokens}`,
+        Authorization: `Bearer ${this.state.inputPersonalAccessToken}`,
       },
     })
       .then(res => {
+        if (res.status === 401) {
+          return console.log('error 401');
+        }
         return res.json();
       })
-      .then(json => {
-        this.setState({ idInfo: json.data.id });
+      .then(res => {
+        console.log('res.data', res.data);
+        this.setState({ idInfo: res.data.id });
+      })
+      // this.state.inputPersonalAccessTokenが適切な値でない場合
+      .catch(error => {
+        console.error(error);
       });
+  }
+
+  changeInput(inputValue: string) {
+    console.log(inputValue);
+    this.setState({ inputPersonalAccessToken: inputValue });
   }
   render(): React.ReactNode {
     return (
-      <div>
+      <>
+        <input
+          type="text"
+          value={this.state.inputPersonalAccessToken}
+          onChange={event => this.changeInput(event.target.value)}
+        />
         <button onClick={() => this.handleClick('user')}>{this.label}</button>
         <p className="IdInfo">{`ID:${this.state.idInfo}`}</p>
-      </div>
+      </>
     );
   }
 }
