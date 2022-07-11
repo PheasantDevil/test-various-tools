9365d02043d98b8da9f23de1cc3990ab1b8ee579
KonishiKenji <konishi.b0engineer@gmail.com>
2022-07-11 23:30:54 +0900

    fix: listen and respond to messages

--- .venv/src/app.py
+++ .venv/src/app.py
@@ -2,8 +2,19 @@ import os
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 
+os.environ['SLACK_BOT_TOKEN'] = 'xoxb-3779134827954-3807082805088-M3fvyFCd1qa1yuSJPdrJSCcy'
+os.environ['SLACK_APP_TOKEN'] = 'xapp-1-A03PDTWMNSV-3785757063316-f0d3d60d1307b07d6dc2c820728ee783c1abeebc1315587f13905896053ba415'
+
 # ボットトークンとソケットモードハンドラーを使ってアプリを初期化します
 app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
+
+# 'hello' を含むメッセージをリッスンします
+# 指定可能なリスナーのメソッド引数の一覧は以下のモジュールドキュメントを参考にしてください：
+# https://slack.dev/bolt-python/api-docs/slack_bolt/kwargs_injection/args.html
+@app.message("hello")
+def message_hello(message, say):
+    # イベントがトリガーされたチャンネルへ say() でメッセージを送信します
+    say(f"Hey there <@{message['user']}>!")
 
 # アプリを起動します
 if __name__ == "__main__":
