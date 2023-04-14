79cbf6a7ed4991424824bd79f64368ce47524d1a
KonishiKenji <konishi.b0engineer@gmail.com>
2023-04-14 22:11:14 +0900

    chore: chatgptに聞いてみた修正

--- src/trial/autoPost.py
+++ src/trial/autoPost.py
@@ -1,6 +1,7 @@
-import os
 import openai
 import requests
+import os
+
 openai.api_key = os.environ.get("OPEN_API_KEY")
 
 def generate_article():
@@ -21,27 +22,38 @@ def generate_article():
     return article
 
 def post_to_hatena_blog(article):
-    # はてなブログに記事を投稿するコードを記述する
-    # 例えば、以下のようにして投稿することができる
-    endpoint = "https://blog.hatena.ne.jp/kb0engineer/k0engineer.hatenablog.com/atom/entry"
+    # はてなブログに記事を投稿するコード
+    title = "記事のタイトルを指定する"
+    body = article
+
+    hatena_blog_username = 'kb0engineer'
+    hatena_blog_api_key = os.environ.get('HATENA_BLOG_API_KEY')
+
+    url = f'https://blog.hatena.ne.jp/{hatena_blog_username}/{hatena_blog_username}.hatenablog.com/atom/entry'
+
     headers = {
-        "Content-Type": "application/xml",
-        "Authorization": "Basic YOUR_API_KEY",
+        'Content-Type': 'application/xml',
+        'X-WSSE': generate_wsse_header(hatena_blog_username, hatena_blog_api_key)
     }
-    data = f"""<?xml version="1.0" encoding="utf-8"?>
-<entry xmlns="http://www.w3.org/2005/Atom">
-    <title>{title}</title>
-    <content type="text/plain">{article}</content>
-    <updated>{datetime.utcnow().isoformat()}Z</updated>
-</entry>
-"""
-    res = requests.post(endpoint, headers=headers, data=data)
 
-    return res.status_code == 201
+    data = f'''<?xml version="1.0" encoding="utf-8"?>
+    <entry xmlns="http://www.w3.org/2005/Atom">
+        <title>{title}</title>
+        <content type="text/plain">{body}</content>
+    </entry>
+    '''
 
-if __name__ == "__main__":
+    response = requests.post(url, headers=headers, data=data.encode('utf-8'))
+    if response.status_code != 201:
+        print(f"Failed to post article: {response.status_code}")
+        return False
+
+    print("Posted to Hatena Blog")
+    return True
+
+if __name__ == '__main__':
     article = generate_article()
     if post_to_hatena_blog(article):
-        print("投稿が完了しました。")
+        exit(0)
     else:
-        print("投稿に失敗しました。")
+        exit(1)
