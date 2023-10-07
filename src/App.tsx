432fc8ff2a55bd9cecd5b6f08633f737e292a4e1
KonishiKenji <konishi.b0engineer@gmail.com>
2023-10-07 15:22:50 +0900

    chore: hide components using timetree api

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,11 @@ import './App.scss';
 import CommonFrame from 'components/molucules/CommonFrame/CommonFrame';
 import ShortcutList from 'components/molucules/shortcut-list/shortcut-list';
 import SlackNotification from 'components/organism/slack/slackNotification';
-import TimeTreeIdInfo from 'components/organism/TimeTree/IdInfo/IdInfo';
 
 function App() {
   return (
     <div className="App">
       {/* <ReactApp /> */}
-      <CommonFrame
-        viewComponent={<TimeTreeIdInfo />}
-        componentName="timetreeIdInfo"
-        frameTitle="TimeTree 認可ユーザーID 取得"
-      />
       <CommonFrame
         viewComponent={<SlackNotification />}
         componentName="slackNotification"
